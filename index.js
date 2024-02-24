566813e524fa01c3db7b9e648120876436f01864
thet htet <thethtetsoe114@gmail.com>
2024-02-24 09:56:59 +0630

    read and write file asyncronoususing callback approach

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const fs = require('fs')
 
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
+//BLOCKING SYNCRONOUS WAY
 
-const textOut = `this is what we know about advacado: ${textIn}`
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
 
-fs.writeFileSync('./txt/output.txt', textOut)
+// const textOut = `this is what we know about advacado: ${textIn}`
 
-console.log('file written!');
+// fs.writeFileSync('./txt/output.txt', textOut)
+
+// console.log('file written!');
+
+
+//NON-BLOCKING ASYNCRONOUS WAY
+
+fs.readFile('./txt/start.txt', 'utf-8', (err,data)=>{
+    if (err) return console.log(err);
+    fs.readFile(`./txt/${data}.txt`, 'utf-8', (err,data1)=>{
+        fs.readFile('./txt/append.txt', 'utf-8', (err,data2) => {
+            fs.writeFile('./txt/final.txt', `${data1}\n ${data2}`, 'utf-8', (err)=>{
+                console.log('file written!');
+            })
+        })
+    })
+})

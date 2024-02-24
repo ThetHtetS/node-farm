e5079cdd1e3d23b7d10666c89ab9714dd8b6cf0a
thet htet <thethtetsoe114@gmail.com>
2024-02-24 12:05:05 +0630

    building very simple api

--- index.js
+++ index.js
@@ -30,9 +30,28 @@ const http = require('http')
 
 //////////////////////////////
 // SERVER
-
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
+const dataObj = JSON.parse(data)
 const server = http.createServer((req, res)=>{
-    res.end('hello from node farm server')
+    const pathName = req.url;
+    if (pathName =='/' || pathName=== '/overview'){
+        console.log(__dirname);
+        res.end('this is an OVERVIEW')
+    } else if(pathName === '/product'){
+        res.end('This is an PRODUCT')
+    }
+    else if(pathName ==='/api'){
+        res.writeHead(404, {
+            'Content-type': 'application/json'
+        })
+        res.end(data)
+    }
+    else {
+        res.writeHead(404, {
+            'Content-type': 'text/html'
+        })
+        res.end('<h1> Page not found </h1>')
+    }
 })
 
 server.listen(8000, '127.0.0.1', ()=>{
